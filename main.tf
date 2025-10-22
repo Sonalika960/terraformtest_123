23b3806fc3afbb960a4716411136c8371344a769
Sonalika960 <sonali.kale0306@gmail.com>
2025-10-22 21:22:30 +0530

    change hw

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws"{
 
 resource "aws_instance" "web01"{
     ami = "ami-0cf8ec67f4b13b491"
-    instance_type = "t3.micro"
+    instance_type = "t2.micro"
 
     tags= {
         Name="demo1"
@@ -15,3 +15,4 @@ output "publicIP"{
     value = aws_instance.web01.public_ip
 
 }
+
