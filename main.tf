66042718ed291aa6dc176edd47148c31d8102a15
Sonalika960 <sonali.kale0306@gmail.com>
2025-10-25 23:01:01 +0530

    V1 updated

--- main.tf
+++ main.tf
@@ -1,20 +1,3 @@
-provider "aws"{
-    region = "ap-south-1"
-}
-
-/* resource "aws_instance" "web01"{
-    ami = "ami-0cf8ec67f4b13b491"
-    instance_type = "t2.micro"
-
-    tags= {
-        Name="tes1"
-    }
-}
-
-output "publicIP"{
-    value = aws_instance.web01.public_ip
-
-}*/
-
+print("hello buddy this is latest v1")
 
 
